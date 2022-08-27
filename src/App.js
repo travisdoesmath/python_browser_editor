11ce91501692a4f0b3ce81a1d116f5620ab66199
Travis Taylor <travisdoesmath@gmail.com>
2022-08-27 14:51:59 -0500

    Added monaco editor and pyodide

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 
-function App() {
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default App;
+export default function App() {
+  const [output, setOutput] = useState("(loading...)");
+  const [code, setCode] = useState("");
+
+  function handleEditorChange(value, event) {
+    setCode(value);
+    //console.log("here is the current model value:", value);
+    //console.log(event);
+  }
+
+  function handleRunCode() {
+    console.log('handleRunCode');
+    window.pyodide.loadPackage([]).then(() => {
+      let output = window.pyodide.runPython(code);
+      console.log('pyodide output', output);
+      setOutput(output);
+    })
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Python in Browser example
+        </Typography>
+        <Editor 
+          height="50vh"
+          defaultLanguage="python"
+          onChange={handleEditorChange}
+          theme="vs-dark"
+        />
+        <Button 
+          onClick={handleRunCode}
+          variant="contained">
+            Run
+        </Button>
+      </Box>
+      <Box>
+        <code>{output}</code>
+      </Box>
+      <Copyright />
+    </Container>
+  );
+}
